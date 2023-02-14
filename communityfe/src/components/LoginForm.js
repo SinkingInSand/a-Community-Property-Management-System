40e4928411aecd3c1e881968b2addbc32f2fb02d
lulu-chenyang <chenyanglulu@gmail.com>
2023-02-14 16:39:02 -0500

    update login form

--- communityfe/src/components/LoginForm.js
+++ communityfe/src/components/LoginForm.js
@@ -4,91 +4,22 @@ import { LockOutlined, UserOutlined } from "@ant-design/icons";
 import { login } from "../utils";
 
 
-const LoginForm =(props)=>{
-  const [isLoggedIn, setLogin] = useState(false);
+// const LoginForm =(props)=>{
+//   const [isLoggedIn, setLogin] = useState(false);
 
-  const handleLogin = () => {
-    setLogin(true);
-    // console.log("login form, isloggedin ", isLoggedIn)
-    props.handelTempLogin(true);
-  }
+//   const handleLogin = () => {
+//     setLogin(true);
+//     // console.log("login form, isloggedin ", isLoggedIn)
+//     props.handelTempLogin(true);
+//   }
   
 
 
 
-  return (
-    <Form
-        name="normal_login"
-        // onFinish={this.onFinish}
-        style={{
-          width: 300,
-          margin: "auto",
-          paddingTop: "100px"
-        }}
-      >
-        <Form.Item
-          name="username"
-          rules={[{ required: true, message: "Please input your Username!" }]}
-        >
-          <Input prefix={<UserOutlined />} placeholder="Username" />
-        </Form.Item>
-        <Form.Item
-          name="password"
-          rules={[{ required: true, message: "Please input your Password!" }]}
-        >
-          <Input.Password prefix={<LockOutlined />} placeholder="Password" />
-        </Form.Item>
-
-        <Form.Item> 
-          {/* <Checkbox>Admin</Checkbox> */}
-          <Button type="primary" onClick={handleLogin}>Login</Button>
-          </Form.Item>
-   
-    </Form>
-  )
-}
-export default LoginForm;
-
-
-
-
-// class LoginForm extends React.Component {
-//   state = {
-//     loading: false,
-//     isAdmin: false,
-//   };
-
-//   onFinish = (data) => {
-//     this.setState({
-//       loading: true,
-//     });
-//     login(data)
-//       .then(() => {
-//         message.success(`Login Successful`);
-//         this.props.onSuccess();
-//       })
-//       .catch((err) => {
-//         message.error(err.message);
-//       })
-//       .finally(() => {
-//         this.setState({
-//           loading: false,
-//         });
-//       });
-//   };
-
-//   onChange = (e) => {
-//     console.log('checked = ', e.target.checked);
-//     this.state = {isAdmin: !this.state.isAdmin}
-//     // this.state.isAdmin(e.target.checked);
-//     console.log("isAdmin? ", this.state.isAdmin);
-//   };
-
-//   render = () => {
-//     return (
-//       <Form
+//   return (
+//     <Form
 //         name="normal_login"
-//         onFinish={this.onFinish}
+//         // onFinish={this.onFinish}
 //         style={{
 //           width: 300,
 //           margin: "auto",
@@ -108,15 +39,87 @@ export default LoginForm;
 //           <Input.Password prefix={<LockOutlined />} placeholder="Password" />
 //         </Form.Item>
 
-//         <Form.Item>
-//           <Checkbox onChange={this.onChange}>Admin</Checkbox>
-//           <Button type="primary" htmlType="submit" loading={this.state.loading}>
-//             Login
-//           </Button>
-//         </Form.Item>
-//       </Form>
-//     );
-//   };
+//         <Form.Item> 
+//           {/* <Checkbox>Admin</Checkbox> */}
+//           <Button type="primary" onClick={handleLogin}>Login</Button>
+//           </Form.Item>
+   
+//     </Form>
+//   )
 // }
-
 // export default LoginForm;
+
+
+const LoginForm =(props)=>{
+  const [isLoggedIn, setLogin] = useState(false);
+  const [loading, setLoading] = useState(false)
+
+  const handleLogin = () => {
+    setLogin(true);
+    // console.log("login form, isloggedin ", isLoggedIn)
+    props.handelTempLogin(true);
+  }
+
+
+
+  const onFinish = (data) => {
+
+    setLoading(true);
+
+    login(data)
+      .then(() => {
+        message.success(`Login Successful`);
+        props.onSuccess();
+      })
+      .catch((err) => {
+        message.error(err.message);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  // const onChange = (e) => {
+  //   console.log('checked = ', e.target.checked);
+
+  //   this.state = {isAdmin: !this.state.isAdmin}
+  //   // this.state.isAdmin(e.target.checked);
+  //   console.log("isAdmin? ", this.state.isAdmin);
+  // };
+
+
+  return (
+    <Form
+      name="normal_login"
+      onFinish={onFinish}
+      style={{
+        width: 300,
+        margin: "auto",
+        paddingTop: "100px"
+      }}
+    >
+      <Form.Item
+        name="username"
+        rules={[{ required: true, message: "Please input your Username!" }]}
+      >
+        <Input prefix={<UserOutlined />} placeholder="Username" />
+      </Form.Item>
+      <Form.Item
+        name="password"
+        rules={[{ required: true, message: "Please input your Password!" }]}
+      >
+        <Input.Password prefix={<LockOutlined />} placeholder="Password" />
+      </Form.Item>
+
+      <Form.Item>
+        {/* <Checkbox onChange={onChange}>Admin</Checkbox> */}
+        <Button type="primary" htmlType="submit" loading={loading}>
+          Login
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+
+}
+
+export default LoginForm;
