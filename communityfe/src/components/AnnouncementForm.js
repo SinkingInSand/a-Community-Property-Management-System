2ab3ceb046d68d982113ffdebdfe4831b07a773f
lulu-chenyang <chenyanglulu@gmail.com>
2023-02-15 12:27:11 -0500

    add post item basic

--- communityfe/src/components/AnnouncementForm.js
+++ communityfe/src/components/AnnouncementForm.js
@@ -1,19 +1,23 @@
 import React from "react";
-import { useState, useEffect } from 'react';
-import { Button} from 'antd';
-import { FileTextOutlined } from '@ant-design/icons';
+import { useState, useEffect } from "react";
+import { Button, Form} from "antd";
+import { FileTextOutlined } from "@ant-design/icons";
 
-const AnnouncementForm = () => {
-    return (
-        // <p>Announcement Form</p>
-    <Button
+
+const AnnouncementForm = (data) => {
+  return (
+    // <p>Announcement Form</p>
+    <>
+      <Form>
+        <Form.Item className="postItem">Post 1</Form.Item>
+      </Form>
+      <Button
         className="floatPost"
-      icon={<FileTextOutlined />}
-      description="Create Post"
-      shape="square"
-
-      
-    />
-    )
+        icon={<FileTextOutlined />}
+        description="Create Post"
+        shape="square"
+      />
+    </>
+  );
 };
 export default AnnouncementForm;
